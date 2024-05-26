43c5a58f178344033091813b812a3219080d8d57
rafaelanggawijaya <124117040+rafaelanggawijaya@users.noreply.github.com>
2024-05-26 14:23:37 +1200

    yes sirrrr

---
+++ 02_menu.v1.py
@@ -0,0 +1,48 @@
+"""Monster cards -menu- v1 This component gives control which part the
+user wants to use and allows the user to pick them
+Update:"""
+
+import easygui
+
+# dictionary of cards
+monster_cards = {"Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25,
+                               "Cunning": 15},
+                 "Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21,
+                               "Cunning": 19},
+                 "Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18,
+                                "Cunning": 22},
+                 "Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23,
+                                "Cunning": 6},
+                 "Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10,
+                              "Cunning": 5},
+                 "Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14,
+                              "Cunning": 5},
+                 "Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19,
+                                "Cunning": 2},
+                 "Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4,
+                              "Cunning": 12},
+                 "Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17,
+                               "Cunning": 4},
+                 "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3,
+                               "Cunning": 2}}
+
+# Main routine
+
+# Welcome message
+easygui.msgbox("Welcome to the Monster card "
+               "Dungeon\n~~~~~~~~~~~~~~~~~~~~~~~~~~", "Welcome Traveler")
+
+option = easygui.buttonbox("Choose what you want to do, Traveler",
+                           "Choose Wisely",
+                           ["Add New Monster", "Search for a Monster",
+                            "Destroy a Monster", "Show all Monsters", "Exit"])
+if option == "Add New Monster":
+    print("Add New monster")
+elif option == "Search for a Monster":
+    print("Search for a monster")
+elif option == "Destroy a Monster":
+    print("Destroy a Monster")
+elif option == "Show all Monsters":
+    print("Show all Monsters")
+else:
+    print("Exit")
