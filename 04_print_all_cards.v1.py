76f05f1a571bfe0a83b3f91e94fc223445ad6d8d
rafaelanggawijaya <124117040+rafaelanggawijaya@users.noreply.github.com>
2024-05-30 14:43:54 +1200

    yes

---
+++ 04_print_all_cards.v1.py
@@ -0,0 +1,65 @@
+"""Monster cards -print_all_cards- v1
+Allows the user to print out all cards in the dictionary
+Update:"""
+
+import easygui
+
+# dictionary of cards
+monster_cards = {"Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25,
+                               "Cunning": 15},
+                 "Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21,
+                               "Cunning": 19},
+                 "Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18,
+                                "Cunning": 22},
+                 "Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23,
+                                "Cunning": 6},
+                 "Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10,
+                              "Cunning": 5},
+                 "Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14,
+                              "Cunning": 5},
+                 "Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19,
+                                "Cunning": 2},
+                 "Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4,
+                              "Cunning": 12},
+                 "Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17,
+                               "Cunning": 4},
+                 "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3,
+                               "Cunning": 2}}
+
+# Main routine
+
+# Welcome message
+easygui.msgbox("Welcome to the Monster card "
+               "Dungeon\n~~~~~~~~~~~~~~~~~~~~~~~~~~", "Welcome Traveler")
+
+# Menu/options
+while True:
+    # asks what the user wants to do
+    option = easygui.buttonbox("Choose what you want to do, Traveler",
+                               "Choose Wisely",
+                               ["Add New Monster", "Search for a Monster",
+                                "Destroy a Monster", "Show all Monsters",
+                                "Exit"])
+    # when user chooses add card
+    if option == "Add New Monster":
+        print("Add New monster")
+    # when user chooses search/edit
+    elif option == "Search for a Monster":
+        print("Search for a monster")
+    # when user chooses delete card
+    elif option == "Destroy a Monster":
+        print("Destroy a Monster")
+    # when user chooses show all cards
+    elif option == "Show all Monsters":
+        # prints out all cards
+        for card, stat in monster_cards.items():
+            # prints card name
+            print(f"{card}\nStats:")
+            for value in stat:
+                # prints stats
+                print(f"    {value}: {stat[value]}")
+            print()
+
+    # when user wants to exit program
+    else:
+        print("Exit")
